fcddfe862877b508f37dd610a37fae4894e61e5e
abdujalil-nizomiddinov <abdujalil2107@gmail.com>
2025-03-16 14:31:00 +0500

    Run code-1

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ num_file = "num.txt"
 if os.path.exists(num_file):
     with open(num_file, "r") as f:
         num = int(f.read().strip())
+        if num >= 35:
+            num = 1
 else:
     num = 1
 
-for i in range(36):    
+for i in range(36):
     os.system("git add .")
     os.system(f'git commit -m "Run code-{num}"')
     os.system("git push")
@@ -18,7 +20,7 @@ for i in range(36):
 
     with open(num_file, "w") as f:
         f.write(str(num))
-    
-    time.sleep(0.5)  
+
+    time.sleep(1)
 
 print("Done ✅")
